ce7fc56c57d417c77685fa576ac7c6badfeb3718
Marina Chernysheva <marich.99@mail.ru>
2024-03-25 21:30:29 +0300

    Added display of data type

--- homework/exercise_3/Chernysheva_1/css/script.js
+++ homework/exercise_3/Chernysheva_1/css/script.js
@@ -5,14 +5,18 @@ function calculate() {
             - Сложение: ${num1 + num2}<br>
             - Вычитание: ${num1 - num2}<br>
             - Умножение: ${num1 * num2}<br>
-            - Деление: ${num1 / num2}`;
+            - Деление: ${num1 / num2}<br><br>
+            Тип данных:<br>
+            - первого числа: ${typeof(num1)}<br>
+            - второго числа: ${typeof(num2)}`;
     document.getElementById('results').innerHTML = results;
 }
 
 function guessNumber() {
     var guessedResult = parseInt(document.getElementById('guessedNumber').value);
     var originalNumber = (guessedResult - 7) / 2;
-    document.getElementById('results').innerHTML = `Задуманное число: ${originalNumber}`;
+    document.getElementById('results').innerHTML = `Задуманное число: ${originalNumber}<br><br>           
+            Тип данных введенного числа: ${typeof(guessedResult)}`;
 }
 
 function calculateAge() {
@@ -20,5 +24,8 @@ function calculateAge() {
     var yearOfBirth = parseInt(document.getElementById('yearOfBirth').value);
     var currentYear = new Date().getFullYear();
     var age = currentYear - yearOfBirth;
-    document.getElementById('results').innerHTML = `${userName}: ${age}`;
+    document.getElementById('results').innerHTML = `${userName}: ${age}<br><br>
+            Тип данных:<br>
+            - имени: ${typeof(userName)}<br>
+            - возраста: ${typeof(yearOfBirth)}`;
 }
